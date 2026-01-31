5671145a03c2a480a371735dccddeee4df143415
Abdullah <itxabdullah44@gmail.com>
2026-01-31 22:19:46 +0500

    initial setup

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,22 @@
+export const signup = async (req, res) => {
+    const {email,password,name}=req.body;
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+export const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+export const logout = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
