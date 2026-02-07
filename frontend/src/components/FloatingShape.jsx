080b2ade3a1e54b7013c4ebd1d2f6d00bb59ccdc
Abdullah <itxabdullah44@gmail.com>
2026-02-08 02:33:14 +0500

    frontend setup

---
+++ frontend/src/components/FloatingShape.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+const FloatingShape = () => {
+  return (
+    <motion.div>FloatingShape</motion.div>
+  )
+}
+
+export default FloatingShape
