7b64168f1abb05f846d1efb7c16ce20a7cd20eab
Abdullah <itxabdullah44@gmail.com>
2026-02-10 15:48:58 +0500

    Page setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 
-
+import FloatingShape from "./components/FloatingShape";
+import {Routes,Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+// import Home from "./pages/Home";
 function App() {
 
   return (
@@ -8,7 +12,11 @@ function App() {
      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
 			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
 			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
-
+      <Routes>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
+      </Routes>
     </div>
   )
 }
